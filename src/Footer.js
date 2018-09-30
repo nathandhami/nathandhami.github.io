a74ad55abd1f7c433e56879bc6fceb5cdd8289d6
nathandhami <nathan_dhami@sfu.ca>
2018-09-29 22:46:00 -0700

    Refactored into components, Centered card

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './footer.css';
+
+export default function footer(props) {
+    return (
+      <footer className="App-footer">
+        Copyright &copy; 2018 Nathan Dhami
+      </footer>
+    );
+}
