7789cc950c721779ad780c8ac29e293c2aa6fc87
nathandhami <nathan_dhami@sfu.ca>
2018-09-30 21:07:41 -0700

    Added sass loader to production webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,6 +212,21 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract(
+              {
+                fallback: 'style-loader',
+                use: ['css-loader',
+                    {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: ["node_modules/"],
+                    },
+                  },
+                ],
+              }),
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
